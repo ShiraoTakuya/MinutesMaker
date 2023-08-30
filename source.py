52540915dae9ced4240ffd5660f29bc837d53b96
ShiraoTakuya <shirao_genu@yahoo.co.jp>
2023-08-30 12:35:41 +0900

    add testMinutes

--- source.py
+++ source.py
@@ -10,7 +10,7 @@ def create_minutes_file(input_text):
     file_name = f"議事録_{today_date}_{input_text}.txt"
 
     # Create and write to the file
-    with open("minutes\\"+file_name, 'w') as file:
+    with open("minutes/"+file_name, 'w') as file:
         file.write(file_name[:-4] + '\n')  # Write the input text on the first line
 
     print(f"ファイル '{file_name}' を作成しました。")
